64df0986437ee7602fa54c50772a8319ce85c908
Garrett Gunnell <gunnellg@oregonstate.edu>
2020-12-26 23:51:01 -0800

    Render image to screen

---
+++ Assets/ImageEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ImageEditor : MonoBehaviour {
+
+    public Texture image;
+    
+    public Shader contrastShader;
+    
+    [Range(0, 10)]
+    public int contrast = 1;
+
+    void OnRenderImage(RenderTexture source, RenderTexture destination) {
+
+        Graphics.Blit(image, destination);
+    }
+}
